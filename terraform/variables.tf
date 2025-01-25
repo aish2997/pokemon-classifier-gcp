5d1e613420305bd727510fa403128fb5c5c92b6c
Aishwarya Bhargava <aish2997@gmail.com>
2025-01-25 10:44:29 +0100

    Fix

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP region"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment (dev/test/prod)"
+}
