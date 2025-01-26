0531b802391b32dfeabe7b0a8285e52c41dbb8fe
Aishwarya Bhargava <aish2997@gmail.com>
2025-01-26 17:55:57 +0100

    Fix

--- terraform/gcs_bucket.tf
+++ terraform/gcs_bucket.tf
@@ -3,3 +3,22 @@ resource "google_storage_bucket" "image_bucket" {
   location      = var.region
   storage_class = "STANDARD"
 }
+
+# Create placeholder files for directory-like structure
+resource "google_storage_bucket_object" "incoming" {
+  name    = "incoming/" # Simulates a directory called 'incoming'
+  bucket  = google_storage_bucket.image_bucket.name
+  content = "" # Empty object
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name    = "archive/" # Simulates a directory called 'archive'
+  bucket  = google_storage_bucket.image_bucket.name
+  content = "" # Empty object
+}
+
+resource "google_storage_bucket_object" "error" {
+  name    = "error/" # Simulates a directory called 'error'
+  bucket  = google_storage_bucket.image_bucket.name
+  content = "" # Empty object
+}
