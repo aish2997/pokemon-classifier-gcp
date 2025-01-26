725d9513beb29116b6978f395105c1d1eeb95801
Aishwarya Bhargava <aish2997@gmail.com>
2025-01-26 12:58:42 +0100

    Fix

--- terraform/cloud_function.tf
+++ terraform/cloud_function.tf
@@ -1,23 +1,36 @@
-resource "google_cloudfunctions_function" "process_image" {
+resource "google_cloudfunctions2_function" "process_image" {
   name        = "process-image"
   description = "Triggered when a new image is uploaded to the GCS bucket."
-  runtime     = "python39"
-  region      = var.region
+  location    = var.region # Gen2 uses `location` instead of `region`.
+  build_config {
+    runtime     = "python39"
+    entry_point = "process_image"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.image_bucket.name
+        object = "pokemon-classifier-d-state/gcs_event_processor.zip"
+      }
+    }
+    environment_variables = {
+      CLOUD_RUN_URL = google_cloud_run_service.image_classifier.status[0].url
+    }
+  }
 
-  available_memory_mb   = 512
-  source_archive_bucket = google_storage_bucket.image_bucket.name
-  source_archive_object = "gcs_event_processor.zip"
-
-  entry_point = "process_image"
-
-  environment_variables = {
-    CLOUD_RUN_URL = google_cloud_run_service.image_classifier.status[0].url
+  service_config {
+    available_memory   = "512Mi" # Memory specification in Gen2
+    min_instance_count = 0       # Default scaling, you can increase if needed
+    max_instance_count = 3000    # Adjust based on your scaling needs
   }
 
   event_trigger {
-    event_type = "google.pubsub.topic.publish"
-    resource   = google_pubsub_topic.image_processing.id
+    event_type = "google.cloud.storage.object.v1.finalized" # GCS trigger for Gen2
+    event_filters {
+      attribute = "bucket"
+      value     = google_storage_bucket.image_bucket.name
+    }
+    trigger_region = var.region # Specify the region for the trigger
   }
+
   depends_on = [
     google_storage_bucket.image_bucket,
     google_pubsub_topic.image_processing
