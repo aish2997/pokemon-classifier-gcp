443ff9b1a41385e1cc0529e400303d1f765b0d65
Aishwarya Bhargava <aish2997@gmail.com>
2025-01-26 18:07:07 +0100

    Fix

--- terraform/cloud_function.tf
+++ terraform/cloud_function.tf
@@ -2,6 +2,7 @@ resource "google_cloudfunctions2_function" "process_image" {
   name        = "process-image"
   description = "Triggered when a new image is uploaded to the GCS bucket."
   location    = var.region # Gen2 uses `location` instead of `region`.
+
   build_config {
     runtime     = "python39"
     entry_point = "process_images" # Ensure this matches the Python function
@@ -11,17 +12,18 @@ resource "google_cloudfunctions2_function" "process_image" {
         object = "gcs_event_processor.zip"
       }
     }
-    environment_variables = {
-      CLOUD_RUN_URL   = google_cloud_run_service.image_classifier.status[0].url
-      GCS_BUCKET_NAME = google_storage_bucket.image_bucket.name
-      BQ_TABLE_ID     = "${google_bigquery_dataset.image_data.dataset_id}.${google_bigquery_table.image_classifications.table_id}"
-    }
   }
 
   service_config {
     available_memory   = "512Mi" # Memory specification in Gen2
-    min_instance_count = 0       # Default scaling, you can increase if needed
-    max_instance_count = 3000    # Adjust based on your scaling needs
+    min_instance_count = 0       # Default scaling
+    max_instance_count = 3000    # Adjust scaling needs
+
+    environment_variables = { # Moved environment variables here
+      CLOUD_RUN_URL   = google_cloud_run_service.image_classifier.status[0].url
+      GCS_BUCKET_NAME = google_storage_bucket.image_bucket.name
+      BQ_TABLE_ID     = "${google_bigquery_dataset.image_data.dataset_id}.${google_bigquery_table.image_classifications.table_id}"
+    }
   }
 
   depends_on = [
