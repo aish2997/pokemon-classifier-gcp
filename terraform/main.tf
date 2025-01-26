4c2694635ec3ebdff331e09f61c5c5366d9f9d32
Aishwarya Bhargava <aish2997@gmail.com>
2025-01-26 11:24:21 +0100

    Fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,3 @@ provider "google" {
   project = var.project_id
   region  = "europe-west1"
 }
-
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
-
-  required_version = ">= 1.3.0"
-}
