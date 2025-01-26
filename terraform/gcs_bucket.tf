130e2c9391fe667d07e1724fe6443b4882b84b4d
Aishwarya Bhargava <aish2997@gmail.com>
2025-01-26 11:45:27 +0100

    Fix

--- terraform/gcs_bucket.tf
+++ terraform/gcs_bucket.tf
@@ -2,10 +2,11 @@ resource "google_storage_bucket" "image_bucket" {
   name          = var.gcs_bucket_name
   location      = var.region
   storage_class = "STANDARD"
+}
 
-  notification {
-    topic          = google_pubsub_topic.image_processing.id
-    event_types    = ["OBJECT_FINALIZE"]
-    payload_format = "JSON_API_V1"
-  }
+resource "google_storage_notification" "gcs_to_pubsub" {
+  bucket         = google_storage_bucket.image_bucket.name
+  topic          = google_pubsub_topic.image_processing.id
+  event_types    = ["OBJECT_FINALIZE"]
+  payload_format = "JSON_API_V1"
 }
