235a527c8ff81a85c7c6d9c0a57480c7c621308b
Aishwarya Bhargava <aish2997@gmail.com>
2025-01-27 02:41:17 +0100

    Fix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,15 @@ output "gcs_bucket_name" {
 output "bigquery_table_id" {
   value = google_bigquery_table.image_classifications.table_id
 }
+
+output "cluster_name" {
+  value = google_container_cluster.primary.name
+}
+
+output "cluster_endpoint" {
+  value = google_container_cluster.primary.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+}
