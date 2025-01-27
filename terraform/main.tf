3e4b057f82b6ba8f47f96cb3a720af6cd2da1040
Aishwarya Bhargava <aish2997@gmail.com>
2025-01-27 02:11:52 +0100

    Fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,5 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  load_config_file = true
 }
-
-data "google_client_config" "default" {}
